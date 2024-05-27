3253949cfbdace5e2b69cb98c2736082b94a1f0d
DiogoRodriguesz <diogomanuelrodrigues04@gmail.com>
2024-05-27 13:26:50 +0100

    Implementation of the application's frontend

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.mooddiary {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.mooddiary to javafx.fxml;
+    exports com.example.mooddiary;
+}
