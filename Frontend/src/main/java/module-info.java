29d47d29c3ee5dee40f0532f4affdb54fad063ad
diogogomesaraujo <118282799+diogogomesaraujo@users.noreply.github.com>
2024-06-01 05:01:36 +0100

    beginning of the frontend

--- Frontend/src/main/java/module-info.java
+++ Frontend/src/main/java/module-info.java
@@ -1,10 +1,13 @@
-module com.example.mooddiary {
+module org.example.frontend {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
-    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
 
-    opens com.example.mooddiary to javafx.fxml;
-    exports com.example.mooddiary;
+    opens org.example.frontend to javafx.fxml;
+    exports org.example.frontend;
 }
